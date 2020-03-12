1cb2f318792256af9b866ecd4e335db9a5018db7
rigonsallauka <62013891+rigonsallauka@users.noreply.github.com>
2020-03-12 10:58:04 +0100

    Excercise 2.3

--- in_out_param.sql
+++ in_out_param.sql
@@ -9,11 +9,14 @@ END;
 --we execute the procedure
 --notice the difference in the usage of IN and OUT parameters
 DECLARE
+i NUMBER(2) := 0;
 empName VARCHAR2(20);
 CURSOR id_cur IS (SELECT emp_id FROM employee);
 BEGIN
   FOR emp_rec IN id_cur LOOP
     emp_name(emp_rec.emp_id,empName);
     DBMS_OUTPUT.PUT_LINE('The employee '||empName||' has id '||emp_rec.emp_id);
+    DBMS_OUTPUT.PUT_LINE(i);
+    i :=i+1;
   END LOOP;
 END;
